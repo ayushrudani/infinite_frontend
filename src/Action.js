ba329f1cb841ad80d26457f0eb455ad2d1809767
ayushrudani <ayushrudani9499@gmail.com>
2024-05-20 11:32:24 +0530

    update Api

--- src/Action.js
+++ src/Action.js
@@ -2,7 +2,9 @@
 
 export const fetchData = async (page) => {
   const response = await fetch(
-    `http://localhost:1572/api/basic-data?pageNo=${parseInt(page)}&pageSize=8`
+    `https://infinite-backend-xp8t.onrender.com/api/basic-data?pageNo=${parseInt(
+      page
+    )}&pageSize=8`
   );
 
   const data = await response.json();
